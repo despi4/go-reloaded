1a46ae56522028baa4ae238888c1cce4a1e5d674
despi4 <johapo62@gmail.com>
2025-09-07 01:06:28 +0500

    first commit

---
+++ auto-correction/hex.go
@@ -0,0 +1,77 @@
+package goreloaded
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"regexp"
+	"strconv"
+)
+
+var hexPattern = `(?i)([0-9a-f]+)\s*\(hex(?:,\d+)?\)`
+
+func HexCommandClear(text string) {
+	var (
+		noDigitRegular   = regexp.MustCompile(`(?i)([0-9a-f]+)\s*\(hex(?:.+)?\)`)
+		withDigitRegular = regexp.MustCompile(`(?i)\s*\(hex(?:.+)?\)`)
+	)
+
+	fmt.Println(noDigitRegular.FindAllString(text, -1))
+
+	fmt.Println(withDigitRegular.FindAllString(text, -1))
+}
+
+func HexToDec(text string) string {
+	regular := regexp.MustCompile(hexPattern)
+	powInt := func(x, y int) int { return int(math.Pow(float64(x), float64(y))) }
+	hexNumbers := map[rune]int{
+		'A': 10,
+		'B': 11,
+		'C': 12,
+		'D': 13,
+		'E': 14,
+		'F': 15,
+	}
+
+	convertedText := regular.ReplaceAllStringFunc(text, func(s string) string {
+		decimal := 0
+		length := -1
+
+		for _, char := range s {
+			if char >= '0' && char <= '9' || char >= 'A' && char <= 'F' || char >= 'a' && char <= 'z' {
+				length++
+			} else {
+				break
+			}
+		}
+
+		for _, char := range s {
+			if length < 0 {
+				break
+			}
+
+			if char >= 'a' && char <= 'z' {
+				char -= 32
+			}
+
+			value, ok := hexNumbers[char]
+			if ok {
+				decimal += value * powInt(16, length)
+				length--
+				continue
+			}
+
+			number, err := strconv.Atoi(string(char))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			decimal += number * powInt(16, length)
+			length--
+		}
+
+		return strconv.Itoa(decimal)
+	})
+
+	return convertedText
+}
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	goreloaded "go-reloaded/auto-correction"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	text := ReadText(CheckInput(args))
+	goreloaded.HexCommandClear(text)
+}
+
+func CheckInput(input []string) string {
+	fileName := ""
+
+	if len(input) == 2 {
+		firstFileName, secondFileName := input[0], input[1]
+
+		if firstFileName[len(firstFileName)-4:] == ".txt" && secondFileName[len(secondFileName)-4:] == ".txt" {
+			_, err := os.Stat(firstFileName)
+			if err != nil {
+				log.Fatal("Input file not exist\n", err)
+			}
+
+			fileName = firstFileName
+		}
+	} else {
+		log.Fatal("Write two [.txt] files, for example | [inputfile.txt] [outputfile.txt]")
+	}
+
+	return fileName
+}
+
+func ReadText(fileName string) string {
+	text, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal("Reading file is unachievable\n", err)
+	}
+
+	return string(text)
+}
